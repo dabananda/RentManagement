564c6b47e8c0eed6dd5e99c1f020d60cbfd803d0
dabananda <dabananda.dev@gmail.com>
2025-09-29 10:32:56 +0600

    feat: extending to identityuser

--- RentManagement.Api/Migrations/20250929042840_AddIdentityTables.Designer.cs
+++ RentManagement.Api/Migrations/20250929042840_AddIdentityTables.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RentManagement.Api.Data;
 
@@ -11,9 +12,11 @@ using RentManagement.Api.Data;
 namespace RentManagement.Api.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250929042840_AddIdentityTables")]
+    partial class AddIdentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ RentManagement.Api/Models/ApplicationUser.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RentManagement.Api.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+    }
+}
