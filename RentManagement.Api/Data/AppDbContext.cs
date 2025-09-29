6ea1473b5f94d15f8605aad46e9370c86a0c672e
dabananda <dabananda.dev@gmail.com>
2025-09-29 09:34:44 +0600

    feat: primary setup and shop model created

---
+++ RentManagement.Api/Data/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using RentManagement.Api.Models;
+
+namespace RentManagement.Api.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Shop> Shops { get; set; }
+    }
+}
---
+++ RentManagement.Api/Models/Shop.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace RentManagement.Api.Models
+{
+    public class Shop
+    {
+        public int Id { get; set; }
+        public required string ShopNumber { get; set; }
+        [Column(TypeName = "decimal(18, 2)")]
+        public decimal AreaSqFt { get; set; }
+        public required string Floor { get; set; }
+        public bool IsOccupied { get; set; }
+    }
+}
