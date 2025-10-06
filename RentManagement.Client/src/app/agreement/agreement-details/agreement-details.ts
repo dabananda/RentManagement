ca86b21da2403cbcd2231214ca59b6af3305b9ed
dabananda <dabananda.dev@gmail.com>
2025-10-06 20:34:57 +0600

    ui: agreement details ui updated

--- RentManagement.Client/src/app/agreement/agreement-details/agreement-details.ts
+++ RentManagement.Client/src/app/agreement/agreement-details/agreement-details.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { AgreementService } from '../../_services/agreement.service';
 import { AgreementDetails as Agreement } from '../../_models/agreement';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-agreement-details',
@@ -13,11 +13,12 @@ export class AgreementDetails implements OnInit {
   agreementService = inject(AgreementService);
   agreement = signal<Agreement | null>(null);
   private route = inject(ActivatedRoute);
+  private router = inject(Router);
 
   id: number = Number(this.route.snapshot.paramMap.get('id'));
 
   ngOnInit(): void {
-    this.loadAgreement()
+    this.loadAgreement();
   }
 
   loadAgreement() {
@@ -25,5 +26,9 @@ export class AgreementDetails implements OnInit {
       next: (agreement) => this.agreement.set(agreement),
       error: (err) => console.log('Error fetching agreement', err),
     });
+  }
+
+  cancel(): void {
+    this.router.navigate(['/agreements']);
   }
 }
